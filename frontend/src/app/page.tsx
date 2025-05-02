b82d8bae2f4653ca718cdc00a69f8880a12fa6e9
Dhruv2550 <dhruvtreddy@gmail.com>
2025-05-02 10:39:07 -0400

    style(remove ID field + style of disabled subscribe button): Removed the session ID field from subscribe section, changed the look of the subscribe button when disabled

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1644,16 +1644,6 @@ export default function Home() {
                         </div>
                       ) : (
                         <div className="flex flex-col gap-5 mt-5 py-1 h-full w-full">
-                          <input
-                            type="number"
-                            placeholder="Session ID"
-                            value={subscribeSessionId}
-                            onChange={(e) =>
-                              setSubscribeSessionId(e.target.value)
-                            }
-                            className="px-2 py-1 rounded-lg border border-slate-300 dark:border-slate-600 text-sm bg-white dark:bg-slate-700 text-slate-800 dark:text-white"
-                          />
-
                           <div className="relative" ref={dropdownRef}>
                             <div
                               onClick={() => setDropdownOpen(!dropdownOpen)}
@@ -1734,21 +1724,14 @@ export default function Home() {
                                 alert("Invalid session ID.");
                                 return;
                               }
-                              if (
-                                sessions.some(
-                                  (s) => s.id === Number(subscribeSessionId)
-                                )
-                              ) {
-                                alert(
-                                  "You are already subscribed to this session."
-                                );
+                              
+                              if (sessions.some((s) => s.id === Number(subscribeSessionId))) {
+                                alert("You are already subscribed to this session.");
                                 return;
                               }
                               try {
                                 const response = await fetch(
-                                  `${process.env.NEXT_PUBLIC_API_URL}/subscribe/${currentUserId}/${Number(
-                                    subscribeSessionId
-                                  )}`,
+                                  `${process.env.NEXT_PUBLIC_API_URL}/subscribe/${currentUserId}/${Number(subscribeSessionId)}`,
                                   {
                                     method: "POST",
                                   }
@@ -1756,15 +1739,10 @@ export default function Home() {
                                 if (response.ok) {
                                   fetchSessions();
                                 } else {
-                                  console.error(
-                                    "Failed to subscribe to session"
-                                  );
+                                  console.error("Failed to subscribe to session");
                                 }
                               } catch (error) {
-                                console.error(
-                                  "Error subscribing to session:",
-                                  error
-                                );
+                                console.error("Error subscribing to session:", error);
                               } finally {
                                 setSubscribeSessionId("");
                                 setSearchSessionInput("");
@@ -1774,7 +1752,8 @@ export default function Home() {
                                 }, 3000);
                               }
                             }}
-                            className="w-full px-3 py-2 bg-blue-600 text-white text-base font-bold rounded-lg hover:bg-blue-700"
+                            className="w-full px-3 py-2 bg-blue-600 text-white text-base font-bold rounded-lg hover:bg-blue-700 disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-blue-600"
+                            disabled={!subscribeSessionId}
                           >
                             Subscribe
                           </button>
