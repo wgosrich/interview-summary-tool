ff738ddecf4bfc0286ceed7838057ca9d6885ffc
Arinjay Singh <arinjay.singh@gmail.com>
2025-05-12 14:45:34 -0400

    chore(backend): restructuring for local db dev

--- backend/myflaskapp/app.py
+++ backend/myflaskapp/app.py
@@ -458,8 +458,9 @@ def load_chat(chat_id):
         "messages": chat.messages
     })
 
-with app.app_context():
-    db.create_all()
-
 if __name__ == "__main__":
+    with app.app_context():
+        print("Creating tables...")
+        db.create_all()
+        print("Done")
     app.run(debug=True, host="0.0.0.0", port=8080)
