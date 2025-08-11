db7747e538ad9585730414a7078ea24059296522
walker <Walker.Gosrich@oit.nj.gov>
2025-08-11 02:04:44 -0400

    remove errored os vars

--- backend/myflaskapp/llm/llm_clients.py
+++ backend/myflaskapp/llm/llm_clients.py
@@ -6,21 +6,35 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # ------------ API CLIENTS ------------ #
-openai_direct=False
-if openai_direct:
-     = os.getenv("OPENAI_GPT4O_API_KEY")
+# openai_direct=False
+# if openai_direct:
+#     openai_gpt4o_api_key = os.getenv("OPENAI_GPT4O_API_KEY")
 
-    if not openai_gpt4o_api_key:
-        raise EnvironmentError(
-            "Missing required environment variables: OPENAI_GPT4O_API_KEY"
-        )
+#     if not openai_gpt4o_api_key:
+#         raise EnvironmentError(
+#             "Missing required environment variables: OPENAI_GPT4O_API_KEY"
+#         )
 
-    gpt4o_client = OpenAI(
-        api_key=openai_gpt4o_api_key
-    )
+#     gpt4o_client = OpenAI(
+#         api_key=openai_gpt4o_api_key
+#     )
 
-    __all__ = ["gpt4o_client"]
+#     __all__ = ["gpt4o_client"]
 
 azure = True
 if azure:
-    azure_api_key = os.getenv("")
+    azure_api_key = os.getenv("AZURE_OPENAI_API_KEY")
+    azure_api_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
+
+    if not azure_api_key or not azure_api_endpoint:
+        raise EnvironmentError(
+            "Missing required environment variables: AZURE_OPENAI_API_KEY or AZURE_OPENAI_ENDPOINT"
+        )
+    
+    az_client = AzureOpenAI(
+        api_key=azure_api_key,
+        api_version="2024-02-01",
+        azure_endpoint=azure_api_endpoint
+    )
+
+    __all__ = ["az_client"]
